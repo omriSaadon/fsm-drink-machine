0a3a84d1e867e7b3ff4dc877a220ba3e5c42578f
Omri Saadon <omri.saadon@samanage.com>
2021-09-19 01:35:22 +0300

    absolute path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DrinkMachine from './components/DrinkMachine/DrinkMachine';
-import MainLayout from './components/Layout/MainLayout';
+import DrinkMachine from 'components/DrinkMachine/DrinkMachine';
+import MainLayout from 'components/Layout/MainLayout';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
